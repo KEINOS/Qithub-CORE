4f5ce12f87da6eb4d6dd59c1f64f1132f7447582
KEINOS <github@keinos.com>
2018-05-05 21:27:12 +0900

    feat: Add sample data for version info

--- src/index.php
+++ src/index.php
@@ -15,3 +15,18 @@ echo MyClass::returnMessage($msg) . PHP_EOL;
 $test = new MyClass();
 $test->echoMessage($msg);
 echo PHP_EOL;
+
+
+/* Box Tips (Git info replacement) */
+/* 以下は Box で Build すると自動的に置き換わります */
+echo '@git_version@' . PHP_EOL;
+echo '@git_commit_long@' . PHP_EOL;
+echo '@git_commit_short@' . PHP_EOL;
+echo '@git_tag@' . PHP_EOL;
+echo '@date_build@' . PHP_EOL;
+
+if (is_phar()) {
+    echo 'Running via phar.' . PHP_EOL;
+} else {
+    echo 'Running PHP directly.' . PHP_EOL;
+}
